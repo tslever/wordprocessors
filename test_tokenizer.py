169dba188f10d731b393c3ae791ccb633d235adf
Tom Lever <thomas.lever.business@gmail.com>
2024-06-06 13:40:18 -0400

    Created class TestTokenizer

--- test_tokenizer.py
+++ test_tokenizer.py
@@ -4,149 +4,160 @@ Module test_tokenizer, which has functions to test cleaning, tokenizing, and cou
 
 
 import logging
+import pytest
 from tokenizer import clean_text, count_words, tokenize
 
 
-logging.basicConfig(
-    filename = "test_tokenizer.log",
-    level = logging.INFO,
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-logger = logging.getLogger(__name__)
+@pytest.fixture(scope = "class", autouse = True)
+def manage_logger():
+    print("Setting up logger")
+
+    logging.basicConfig(
+        filename = "test_tokenizer.log",
+        level = logging.INFO,
+        format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+    
+    yield logger
+    
+    print("Tearing down logger")
 
 
-def test_clean_text():
-    '''
-    Tests cleaning text
+class TestTokenizer:
 
-    Keyword arguments:
-        none
+    def test_clean_text(self):
+        '''
+        Tests cleaning text
 
-    Return values:
-        none
+        Keyword arguments:
+            none
 
-    Side effects:
-        Compares actual and expected cleaned texts
+        Return values:
+            none
 
-    Exceptions raised:
-        AssertionError if actual cleaned text does not equal expected cleaned text
+        Side effects:
+            Compares actual and expected cleaned texts
 
-    Restrictions on when this method can be called:
-        none
-    '''
+        Exceptions raised:
+            AssertionError if actual cleaned text does not equal expected cleaned text
 
-    text_to_clean = """And he looked over at the alarm clock,
-    ticking on the chest of drawers. "God in Heaven!" he thought.
-    It was half past six and the hands were quietly moving forwards,
-    it was even later than half past, more like quarter to seven.
-    Had the alarm clock not rung? He could see from the bed that it
-    had been set for four o'clock as it should have been; it certainly must have rung.
-    Yes, but was it possible to quietly sleep through that furniture-rattling noise?
-    True, he had not slept peacefully, but probably all the more deeply because of that"""
+        Restrictions on when this method can be called:
+            none
+        '''
 
-    actual_cleaned_text = clean_text(text_to_clean)
+        text_to_clean = """And he looked over at the alarm clock,
+        ticking on the chest of drawers. "God in Heaven!" he thought.
+        It was half past six and the hands were quietly moving forwards,
+        it was even later than half past, more like quarter to seven.
+        Had the alarm clock not rung? He could see from the bed that it
+        had been set for four o'clock as it should have been; it certainly must have rung.
+        Yes, but was it possible to quietly sleep through that furniture-rattling noise?
+        True, he had not slept peacefully, but probably all the more deeply because of that"""
 
-    expected_cleaned_text = """and he looked over at the alarm clock
-    ticking on the chest of drawers god in heaven he thought
-    it was half past six and the hands were quietly moving forwards
-    it was even later than half past more like quarter to seven
-    had the alarm clock not rung he could see from the bed that it
-    had been set for four oclock as it should have been it certainly must have rung
-    yes but was it possible to quietly sleep through that furniturerattling noise
-    true he had not slept peacefully but probably all the more deeply because of that"""
+        actual_cleaned_text = clean_text(text_to_clean)
 
-    logger.info("Testing clean_text")
+        expected_cleaned_text = """and he looked over at the alarm clock
+        ticking on the chest of drawers god in heaven he thought
+        it was half past six and the hands were quietly moving forwards
+        it was even later than half past more like quarter to seven
+        had the alarm clock not rung he could see from the bed that it
+        had been set for four oclock as it should have been it certainly must have rung
+        yes but was it possible to quietly sleep through that furniturerattling noise
+        true he had not slept peacefully but probably all the more deeply because of that"""
 
-    assert \
-        actual_cleaned_text == expected_cleaned_text, \
-        "Actual cleaned text is not equal to expected cleaned text."
+        logger.info("Testing clean_text")
 
-
-def test_tokenize():
-    '''
-    Tests tokenizing text
-
-    Keyword arguments:
-        none
-
-    Return values:
-        none
-
-    Side effects:
-        Compares actual and expected lists of words from text
-
-    Exceptions raised:
-        AssertionError if actual list of words does not equal expected list of words
-
-    Restrictions on when this method can be called:
-        none
-    '''
-
-    text_to_tokenize = "This is a vast world you can't traverse world in a day"
-
-    actual_list_of_words = tokenize(text_to_tokenize)
-
-    expected_list_of_words = [
-        "This",
-        "is",
-        "a",
-        "vast",
-        "world",
-        "you",
-        "can't",
-        "traverse",
-        "world",
-        "in",
-        "a",
-        "day"
-    ]
-
-    logger.info("Testing tokenize")
-
-    assert \
-        actual_list_of_words == expected_list_of_words, \
-        "Actual list of words is not equal to expected list of words."
+        assert \
+            actual_cleaned_text == expected_cleaned_text, \
+            "Actual cleaned text is not equal to expected cleaned text."
 
 
-def test_count_words():
-    '''
-    Tests providing a dictionary of words in text and the counts of those words
+    def test_tokenize(self):
+        '''
+        Tests tokenizing text
 
-    Keyword arguments:
-        none
+        Keyword arguments:
+            none
 
-    Return values:
-        none
+        Return values:
+            none
 
-    Side effects:
-        Compares actual and expected dictionaries of words and counts
+        Side effects:
+            Compares actual and expected lists of words from text
 
-    Exceptions raised:
-        AssertionError if actual dictionary of words and counts does not equal expected dictionary
+        Exceptions raised:
+            AssertionError if actual list of words does not equal expected list of words
 
-    Restrictions on when this method can be called:
-        none
-    '''
+        Restrictions on when this method can be called:
+            none
+        '''
 
-    text_of_which_to_count_words = "This is a vast world you can't traverse world in a day"
+        text_to_tokenize = "This is a vast world you can't traverse world in a day"
 
-    actual_dictionary_of_words_and_counts = count_words(text_of_which_to_count_words)
+        actual_list_of_words = tokenize(text_to_tokenize)
 
-    expected_dictionary_of_words_and_counts = {
-        'a': 2,
-        'world': 2,
-        "can't": 1,
-        'day': 1,
-        'traverse': 1, 
-        'is': 1,
-        'vast': 1,
-        'in': 1,
-        'you': 1,
-        'This': 1
-    }
+        expected_list_of_words = [
+            "This",
+            "is",
+            "a",
+            "vast",
+            "world",
+            "you",
+            "can't",
+            "traverse",
+            "world",
+            "in",
+            "a",
+            "day"
+        ]
 
-    logger.info("Testing count_words")
+        logger.info("Testing tokenize")
 
-    assert \
-        actual_dictionary_of_words_and_counts == expected_dictionary_of_words_and_counts, \
-        "Actual dictionary of words and counts does not equal expected dictionary."
+        assert \
+            actual_list_of_words == expected_list_of_words, \
+            "Actual list of words is not equal to expected list of words."
+
+
+    def test_count_words(self):
+        '''
+        Tests providing a dictionary of words in text and the counts of those words
+
+        Keyword arguments:
+            none
+
+        Return values:
+            none
+
+        Side effects:
+            Compares actual and expected dictionaries of words and counts
+
+        Exceptions raised:
+            AssertionError if actual dictionary of words and counts does not equal expected dictionary
+
+        Restrictions on when this method can be called:
+            none
+        '''
+
+        text_of_which_to_count_words = "This is a vast world you can't traverse world in a day"
+
+        actual_dictionary_of_words_and_counts = count_words(text_of_which_to_count_words)
+
+        expected_dictionary_of_words_and_counts = {
+            'a': 2,
+            'world': 2,
+            "can't": 1,
+            'day': 1,
+            'traverse': 1, 
+            'is': 1,
+            'vast': 1,
+            'in': 1,
+            'you': 1,
+            'This': 1
+        }
+
+        logger.info("Testing count_words")
+
+        assert \
+            actual_dictionary_of_words_and_counts == expected_dictionary_of_words_and_counts, \
+            "Actual dictionary of words and counts does not equal expected dictionary."
