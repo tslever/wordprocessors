20421a36ef631dde003dd531c1eda103fa104526
Tom Lever <thomas.lever.business@gmail.com>
2024-06-14 15:47:06 -0400

    Parameterized test_clean_text

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -3,11 +3,19 @@ Module test_cleaner, which has functions to test cleaning text
 '''
 
 
-from logger import logger
 from tsl2b_DS5111su24_lab_01.word_processors import clean_text
+from logger import logger
+import pytest
 
 
-def test_clean_text(logger):
+@pytest.mark.parametrize(
+    "quote_from_The_Raven",
+    [
+        ("But the Raven, sitting lonely on the placid bust, spoke only That one word, as if his soul in that one word he did outpour."),
+        ("But the Raven-- sitting lonely on the placid bust --spoke only That one word, as if his soul in that one word he did outpour.")
+    ]
+)
+def test_clean_text(logger, quote_from_The_Raven):
     '''
     Given a string text_to_clean of text with words,
     when I pass text_to_clean to function clean_text,
@@ -33,9 +41,7 @@ def test_clean_text(logger):
 
     logger.info("Testing cleaning text")
 
-    text_to_clean = """But the Raven, sitting lonely on the placid bust, spoke only That one word, as if his soul in that one word he did outpour."""
-
-    actual_cleaned_text = clean_text(text_to_clean)
+    actual_cleaned_text = clean_text(quote_from_The_Raven)
 
     expected_cleaned_text = """but the raven sitting lonely on the placid bust spoke only that one word as if his soul in that one word he did outpour"""
 
