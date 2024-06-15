54a362699b89df622ef015a529e4c40e609b659f
Tom Lever <thomas.lever.business@gmail.com>
2024-06-15 01:32:44 -0400

    Tested counting words for The Raven

--- tests/test_word_counter.py
+++ tests/test_word_counter.py
@@ -5,6 +5,7 @@ Module test_word_counter, which has a function to test counting words in text
 
 from fixtures import logger, quote_from_The_Raven
 from tsl2b_DS5111su24_lab_01.word_processors import clean_text, count_words, tokenize
+import pickle
 import pytest
 
 
@@ -65,6 +66,50 @@ def test_count_words(logger, quote_from_The_Raven):
     assert \
         actual_dictionary_of_words_and_counts == expected_dictionary_of_words_and_counts, \
         f"Given text {text_of_which_to_count_words}, actual dictionary of words and counts does not equal expected dictionary."
+
+
+def test_count_words_in_The_Raven(logger):
+    '''
+    Given a string text_of_which_to_count_words
+    that is cleanish according to a restriction on using function count_words,
+    when I pass text_of_which_to_count_words to count_words,
+    I should get a dictionary of those words and their counts.
+    The words should consist of lowercase characters not in string.punctuation.
+
+    Keyword arguments:
+        none
+
+    Return values:
+        none
+
+    Side effects:
+        Compares actual and expected dictionaries of words and counts
+
+    Exceptions raised:
+        AssertionError if actual dictionary does not equal expected dictionary
+
+    Restrictions on when this method can be called:
+        none
+    '''
+
+    logger.info("Testing counting words")
+
+    text = None
+    with open("The_Raven.txt", 'r') as file:
+        text = file.read()
+
+    text_of_which_to_count_words = clean_text(text)
+
+    actual_dictionary_of_words_and_counts = count_words(text_of_which_to_count_words)
+
+    expected_dictionary_of_words_and_counts = None
+    with open("Dictionary_Of_Words_And_Counts_For_The_Raven.pickle", "rb") as file:
+        expected_dictionary_of_words_and_counts = pickle.load(file)
+
+    assert \
+        actual_dictionary_of_words_and_counts == expected_dictionary_of_words_and_counts, \
+        f"Given text {text_of_which_to_count_words}, actual dictionary of words and counts does not equal expected dictionary."
+
 
 
 def test_that_there_are_21_unique_words(logger, quote_from_The_Raven):
