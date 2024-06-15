ddc40cf7f64fa6ee3044e6ec3f7a9cecf0687ee2
Tom Lever <thomas.lever.business@gmail.com>
2024-06-15 16:16:09 -0400

    Improved documentation

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,29 +1,32 @@
 '''
-Module test_tokenizer, which has a function to test tokenizing text
+Module test_tokenizer, which has functions to test tokenizing text
 '''
 
 
-from fixtures import logger, quote_from_The_Raven
-from tsl2b_DS5111su24_lab_01.word_processors import clean_text, tokenize
+from tsl2b_DS5111su24_lab_01.word_processors import clean_text
+from fixtures import logger
+import pickle
 import pytest
+from fixtures import quote_from_The_Raven
+from tsl2b_DS5111su24_lab_01.word_processors import tokenize
 
 
 def test_tokenize(logger, quote_from_The_Raven):
     '''
-    Given a string quote_from_The_Raven of text with words
-    that is cleanish according to a restriction on using function tokenize,
+    Given a string quote_from_The_Raven of text with words from The Raven,
     when I pass a cleaned version of quote_from_The_Raven to tokenize,
     I should get a list of the words in the version as return.
     The words should consist of lowercase characters not in string.punctuation.
 
     Keyword arguments:
-        none
+        logger: Logger -- a logger
+        quote_from_The_Raven: str -- a quote from The Raven
 
     Return values:
         none
 
     Side effects:
-        Compares actual and expected lists of words from text
+        Compares actual and expected lists of words from quote
 
     Exceptions raised:
         AssertionError if actual list of words does not equal expected list of words
@@ -68,30 +71,27 @@ def test_tokenize(logger, quote_from_The_Raven):
 
     assert \
         actual_list_of_words == expected_list_of_words, \
-        f"Given text {text_to_tokenize}, actual list of words is not equal to expected list of words."
+        f"Actual list of words in quote from The Raven is not equal to expected list of words in quote from The Raven."
 
-
-import pickle
 
 def test_tokenize_The_Raven(logger):
     '''
-    Given a string of text with words
-    that is cleanish according to a restriction on using function tokenize,
+    Given a string of text with words from The Raven,
     when I pass a cleaned version of the text to tokenize,
     I should get a list of the words in the version as return.
     The words should consist of lowercase characters not in string.punctuation.
 
     Keyword arguments:
-        none
+        logger: Logger -- a logger
 
     Return values:
         none
 
     Side effects:
-        Compares actual and expected lists of words from text
+        Compares actual and expected lists of words from The Raven
 
     Exceptions raised:
-        AssertionError if actual list of words does not equal expected list of words
+        AssertionError if actual list of words from The Raven does not equal expected list of words from The Raven
 
     Restrictions on when this method can be called:
         none
@@ -112,20 +112,21 @@ def test_tokenize_The_Raven(logger):
     
     assert \
         actual_list_of_words == expected_list_of_words, \
-        f"Given text, actual list of words is not equal to expected list of words."
+        f"Actual list of words from The Raven is not equal to expected list of words from The Raven."
 
 
 
-def test_that_there_are_no_hyphens(logger, quote_from_The_Raven):
+def test_that_there_are_no_hyphens_in_words_in_cleaned_version_of_quote_from_The_Raven(logger, quote_from_The_Raven):
     '''
-    Given a string quote_from_The_Raven of text with words
-    that is cleanish according to a restriction on using function tokenize,
-    when I pass a version of quote_from_The_Raven to tokenize,
+    Given a string quote_from_The_Raven of text with words from The Raven,
+    when I pass a cleaned version of quote_from_The_Raven to tokenize,
     I should get a list of the words in the version as return.
     The words should consist of lowercase characters not in string.punctuation.
+    The words should not have hyphens.
 
     Keyword arguments:
-        none
+        logger: Logger -- a logger
+        quote_from_The_Raven: str -- a quote from The Raven
 
     Return values:
         none
@@ -140,7 +141,7 @@ def test_that_there_are_no_hyphens(logger, quote_from_The_Raven):
         none
     '''
 
-    logger.info("Testing that there are no hyphens in cleaned version of quote from The Raven")
+    logger.info("Testing that there are no hyphens in words in cleaned version of quote from The Raven")
 
     text_to_tokenize = clean_text(quote_from_The_Raven)
 
@@ -153,20 +154,21 @@ def test_that_there_are_no_hyphens(logger, quote_from_The_Raven):
 
     assert \
         there_are_no_hyphens, \
-        f"Given text {quote_from_The_Raven}, there are no hyphens in the words in a cleaned version of this text."
+        f"There is a hyphen in a word in a cleaned version of a quote from The Raven."
 
 
 @pytest.mark.xfail
-def test_that_a_word_has_a_hyphen(logger, quote_from_The_Raven):
+def test_that_a_word_in_cleaned_version_of_quote_from_The_Raven_has_a_hyphen(logger, quote_from_The_Raven):
     '''
-    Given a string quote_from_The_Raven of text with words
-    that is cleanish according to a restriction on using function tokenize,
+    Given a string quote_from_The_Raven of text with words from The Raven,
     when I pass a cleaned version of quote_from_The_Raven to tokenize,
     I should get a list of the words in the version as return.
     The words should consist of lowercase characters not in string.punctuation.
+    The words should not have hyphens; a test that a word has a hyphen should fail.
 
     Keyword arguments:
-        none
+        logger: Logger -- a logger
+        quote_from_The_Raven: str -- a quote from The Raven
 
     Return values:
         none
@@ -187,7 +189,6 @@ def test_that_a_word_has_a_hyphen(logger, quote_from_The_Raven):
 
     actual_list_of_words = tokenize(text_to_tokenize)
 
-
     there_are_no_hyphens = True
     for word in actual_list_of_words:
         if '-' in word:
@@ -195,5 +196,5 @@ def test_that_a_word_has_a_hyphen(logger, quote_from_The_Raven):
 
     assert \
         not there_are_no_hyphens, \
-        f"Given text {quote_from_The_Raven}, there are no hyphens in the words in a cleaned version of this text."
+        f"There are no hyphens in the words in a cleaned version of a quote from The Raven."
 
