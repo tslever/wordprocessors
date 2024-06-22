995316dc0f02b2ade5120507d521d7b51f599788
Tom Lever <thomas.lever.business@gmail.com>
2024-06-22 01:02:36 -0400

    Got file name instead of base name

--- clean_text.sh
+++ clean_text.sh
@@ -1,1 +1,1 @@
-cat "$1" | gawk '{print tolower($0)}' | tr -d "\!\"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~" > "$1_Cleaned_By_Command.txt"
+cat "$1" | gawk '{print tolower($0)}' | tr -d "\!\"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~" > "${$1%.*}_Cleaned_By_Command.txt"
