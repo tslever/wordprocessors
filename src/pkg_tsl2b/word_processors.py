cd7d913fa62f72eb6a0f5323e0a009bd237f27fe
Tom Lever <thomas.lever.business@gmail.com>
2024-07-15 19:49:37 -0400

    Addressing pylint's warnings for module word_processors

--- src/pkg_tsl2b/word_processors.py
+++ src/pkg_tsl2b/word_processors.py
@@ -5,23 +5,22 @@ Module word_processors, which has functions to clean, tokenize, and count words 
 
 import argparse
 from collections import Counter
-from typing import Dict
+from typing import Dict, List
 import logging
 import os
 import pickle
 import string
-from typing import List
 
 
 logger = logging.getLogger(__name__)
 
 
-def clean_text(text: str) -> str:
+def clean_text(text_to_clean: str) -> str:
     '''
     Cleans text by lowercasing and removing characters in an augmentation of string.punctuation
         
     Keyword arguments:
-        text: str -- text
+        text_to_clean: str -- text to clean
 
     Return values:
         cleaned_text: str -- cleaned text
@@ -36,10 +35,10 @@ def clean_text(text: str) -> str:
         none
     '''
 
-    assert isinstance(text, str)
+    assert isinstance(text_to_clean, str)
 
     translation_table = str.maketrans('', '', string.punctuation + "\r«»")
-    cleaned_text = text.lower().translate(translation_table)
+    cleaned_text = text_to_clean.lower().translate(translation_table)
 
     #logger.info("Cleaned text: %s", cleaned_text)
 
@@ -49,12 +48,12 @@ def clean_text(text: str) -> str:
     return cleaned_text
 
 
-def tokenize(text: str) -> List[str]:
+def tokenize(text_to_tokenize: str) -> List[str]:
     '''
     Tokenizes text of words separated by spaces into a list of those words
 
     Keyword arguments:
-        text: str -- text
+        text_to_tokenize: str -- text to tokenize
 
     Return values:
         list_of_words: List[str] -- list of words
@@ -66,12 +65,12 @@ def tokenize(text: str) -> List[str]:
         none
 
     Restrictions on when this method can be called:
-        text should be cleanish.
+        text to tokenize should be cleanish.
     '''
 
-    assert isinstance(text, str)
+    assert isinstance(text_to_tokenize, str)
 
-    list_of_words = text.split()
+    list_of_words = text_to_tokenize.split()
 
     #logger.info("Tokenized text: %s", list_of_words)
 
@@ -81,15 +80,15 @@ def tokenize(text: str) -> List[str]:
     return list_of_words
 
 
-def count_words(text: str) -> Dict[str, int]:
+def count_words(text_for_counting_words: str) -> Dict[str, int]:
     '''
     Provides a dictionary of the words in specified text and the counts of those words
 
     Keyword arguments:
-        text: str -- text
+        text_for_counting_words: str -- text
 
     Return values:
-        dictionary_of_words_and_counts: Dict[str, int] -- dictionary of words in specified text and counts
+        dictionary_of_words_and_counts: Dict[str, int] -- dictionary of words in text and counts
 
     Side effects:
         none
@@ -98,59 +97,84 @@ def count_words(text: str) -> Dict[str, int]:
         none
 
     Restrictions on when this method can be called:
-        text should be cleanish.
+        text for counting words should be cleanish.
     '''
 
-    assert isinstance(text, str)
+    assert isinstance(text_for_counting_words, str)
 
-    list_of_words = tokenize(text)
+    list_of_words = tokenize(text_for_counting_words)
     the_counter = Counter(list_of_words)
-    dictionary_of_words_and_counts = dict(the_counter)
+    dictionary = dict(the_counter)
 
-    #logger.info("Counted words: %s", dictionary_of_words_and_counts)
+    #logger.info("Counted words: %s", dictionary)
 
-    assert not dictionary_of_words_and_counts is None
-    assert isinstance(dictionary_of_words_and_counts, dict)
+    assert not dictionary is None
+    assert isinstance(dictionary, dict)
 
-    return dictionary_of_words_and_counts
+    return dictionary
 
 
 def parse_arguments():
-    dictionary_of_arguments = {}
-    parser = argparse.ArgumentParser(prog = "Word Processors", description = "This program processes text.")
+    '''
+    Parses command line arguments into a dictionary of argument names and values
+
+    Keyword arguments:
+        none
+
+    Return values:
+        dictionary_of_arguments: Dict[str, int] -- dictionary of argument names and values
+
+    Side effects:
+        none
+
+    Exceptions raised:
+        none
+
+    Restrictions on when this method can be called:
+        first when this script is executed
+    '''
+
+    dictionary = {}
+    parser = argparse.ArgumentParser(
+        prog = "Word Processors",
+        description = "This program processes text."
+    )
     parser.add_argument("name_of_function", help = "name of function")
     parser.add_argument("path_to_file_of_text", help = "path to file of text")
     args = parser.parse_args()
-    name_of_function = args.name_of_function
-    path_to_file_of_text = args.path_to_file_of_text
-    print(f"name of function: {name_of_function}")
-    print(f"path to file of text: {path_to_file_of_text}")
-    dictionary_of_arguments["name_of_function"] = name_of_function
-    dictionary_of_arguments["path_to_file_of_text"] = path_to_file_of_text
-    return dictionary_of_arguments
+    function_name = args.name_of_function
+    path_to_text_file = args.path_to_file_of_text
+    print(f"name of function: {function_name}")
+    print(f"path to file of text: {path_to_text_file}")
+    dictionary["name_of_function"] = function_name
+    dictionary["path_to_file_of_text"] = path_to_text_file
+    return dictionary
 
 if __name__ == "__main__":
     dictionary_of_arguments = parse_arguments()
     name_of_function = dictionary_of_arguments["name_of_function"]
     path_to_file_of_text = dictionary_of_arguments["path_to_file_of_text"]
-    text = None
-    with open(path_to_file_of_text, 'r') as file:
-        text = file.read()
+    TEXT = None
+    with open(path_to_file_of_text, 'r', encoding = "utf-8") as file:
+        TEXT = file.read()
     base_name = os.path.basename(path_to_file_of_text)
     file_name, extension = os.path.splitext(base_name)
     if name_of_function == "clean_text":
-        cleaned_text = clean_text(text)
-        with open(f"{file_name}_Cleaned{extension}", 'w') as file:
-            file.write(cleaned_text)
+        clean_text = clean_text(TEXT)
+        with open(f"{file_name}_Cleaned{extension}", 'w', encoding = "utf-8") as file:
+            file.write(clean_text)
     elif name_of_function == "tokenize":
-        cleaned_text = clean_text(text)
-        list_of_words = tokenize(cleaned_text)
+        clean_text = clean_text(TEXT)
+        list_of_tokenized_words = tokenize(clean_text)
         with open(f"List_Of_Words_In_Cleaned_Version_Of_{file_name}.pickle", "wb") as file:
-            pickle.dump(list_of_words, file)
+            pickle.dump(list_of_tokenized_words, file)
     elif name_of_function == "count_words":
-        cleaned_text = clean_text(text)
-        dictionary_of_words_and_counts = count_words(cleaned_text)
-        with open(f"Dictionary_Of_Words_And_Counts_For_Cleaned_Version_Of_{file_name}.pickle", "wb") as file:
+        clean_text = clean_text(TEXT)
+        dictionary_of_words_and_counts = count_words(clean_text)
+        with open(
+            f"Dictionary_Of_Words_And_Counts_For_Cleaned_Version_Of_{file_name}.pickle",
+            "wb"
+        ) as file:
             pickle.dump(dictionary_of_words_and_counts, file)
     else:
         raise ValueError(f"Name of function \"{name_of_function}\" is invalid.")
